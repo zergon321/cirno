a539a89d77be2608eef24c7a942fc815f74c283f
zergon321 <maximgradan@gmail.com>
2020-02-26 00:08:21 +0300

    Tag type added.

---
+++ tag.go
@@ -0,0 +1,21 @@
+package cirno
+
+// Tag identifies the kind of the shape
+// to collide other shapes.
+type Tag int32
+
+// ShouldCollide returns true if the shape should
+// collide another one accodring to their tags.
+func (tag Tag) ShouldCollide(other Tag) bool {
+	return tag&other > 0
+}
+
+// SetTag assign a new value to the tag.
+func (tag *Tag) SetTag(newTag Tag) {
+	*tag = newTag
+}
+
+// GetTag returns the value if the tag.
+func (tag Tag) GetTag() Tag {
+	return tag
+}
