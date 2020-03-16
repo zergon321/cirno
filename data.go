fb084bdd20001541ff45b960a036a3b35f7654f9
zergon321 <maximgradan@gmail.com>
2020-03-16 04:35:05 +0300

    Comments added.

--- data.go
+++ data.go
@@ -1,13 +1,18 @@
 package cirno
 
+// data contains any data
+// assigned to the shape.
 type data struct {
 	data interface{}
 }
 
+// Data returns whatever is saved in the
+// data field of the shape.
 func (data *data) Data() interface{} {
 	return data.data
 }
 
+// SetData assigns new data to the shape.
 func (data *data) SetData(newData interface{}) {
 	data.data = newData
 }
