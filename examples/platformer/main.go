725997ce3026fd6e503a0b3c1013aece5a219f00
zergon321 <maximgradan@gmail.com>
2020-03-23 19:24:37 +0300

    Code extended.

--- examples/platformer/main.go
+++ examples/platformer/main.go
@@ -14,8 +14,11 @@ import (
 )
 
 const (
-	width  = 1280
-	height = 720
+	width         = 1280
+	height        = 720
+	platformID    = 1
+	beholderID    = 1 << 1
+	beholderEyeID = 1 << 3
 )
 
 var (
@@ -27,6 +30,30 @@ type platform struct {
 	sprite    *pixel.Sprite
 	transform pixel.Matrix
 	batch     *pixel.Batch
+}
+
+type beholder struct {
+	rect      *cirno.Rectangle
+	hitCircle *cirno.Circle
+	sprite    *pixel.Sprite
+	transform pixel.Matrix
+	batch     *pixel.Batch
+	dead      bool
+}
+
+type bullet struct {
+	hitShape  *cirno.Shape
+	sprite    *pixel.Sprite
+	transform pixel.Matrix
+	batch     *pixel.Batch
+	direction cirno.Vector
+}
+
+type player struct {
+	rect      *cirno.Rectangle
+	sprite    *pixel.Sprite
+	transform pixel.Matrix
+	dead      bool
 }
 
 func loadPicture(pic string) (pixel.Picture, error) {
@@ -72,14 +99,75 @@ func run() {
 	handleError(err)
 	projectileSheet, err := loadPicture("projectiles.png")
 	handleError(err)
-	beholderPic, err := loadPicture("beholder.png")
+	beholderPic, err := loadPicture("beholders.png")
+	handleError(err)
+	testmanPic, err := loadPicture("testmen.png")
 	handleError(err)
 
 	// Create sprites and batches.
 	wallSprite := pixel.NewSprite(wallPic, pixel.R(0, 0, width, height))
+	testmanLeftSprite := pixel.NewSprite(testmanPic, pixel.R(0, 0, 32, 64))
+	testmanRightSprite := pixel.NewSprite(testmanPic, pixel.R(32, 0, 64, 64))
+	electroBulletSprite := pixel.NewSprite(projectileSheet, pixel.R(0, 0, 64, 64))
+	bloodBulletSprite := pixel.NewSprite(projectileSheet, pixel.R(64, 0, 192, 64))
+	platformSprite := pixel.NewSprite(platformPic, pixel.R(0, 0, 128, 32))
+	beholderLeftSprite := pixel.NewSprite(beholderPic, pixel.R(0, 0, 129, 315))
+	beholderRightSprite := pixel.NewSprite(beholderPic, pixel.R(129, 0, 258, 315))
+
 	platformBatch := pixel.NewBatch(new(pixel.TrianglesData), platformPic)
-	projectileBatch := pixel.NewBatch(new(pixel.TrianglesData), projectileSheet)
+	bulletBatch := pixel.NewBatch(new(pixel.TrianglesData), projectileSheet)
 	beholderBatch := pixel.NewBatch(new(pixel.TrianglesData), beholderPic)
+
+	// Create a new collision space.
+	space := cirno.NewSpace(5, 20, width*2, height*2,
+		cirno.Zero, cirno.NewVector(width, height), true)
+
+	// Create platforms.
+	lowerPlatform := platform{
+		rect:      cirno.NewRectangle(cirno.NewVector(640, 180), 128, 32, 0),
+		sprite:    platformSprite,
+		transform: pixel.IM.Moved(pixel.V(640, 180)),
+		batch:     platformBatch,
+	}
+	middlePlatform := platform{
+		rect:      cirno.NewRectangle(cirno.NewVector(320, 360), 128, 32, 0),
+		sprite:    platformSprite,
+		transform: pixel.IM.Moved(pixel.V(320, 360)),
+		batch:     platformBatch,
+	}
+	higherPlatform := platform{
+		rect:      cirno.NewRectangle(cirno.NewVector(960, 540), 128, 32, 0),
+		sprite:    platformSprite,
+		transform: pixel.IM.Moved(pixel.V(960, 540)),
+		batch:     platformBatch,
+	}
+
+	lowerPlatform.rect.SetIdentity(platformID)
+	middlePlatform.rect.SetIdentity(platformID)
+	higherPlatform.rect.SetIdentity(platformID)
+
+	// Create beholders.
+	lowerBeholder := beholder{
+		rect:      cirno.NewRectangle(cirno.NewVector(320, 534), 129, 315, 0),
+		hitCircle: cirno.NewCircle(cirno.NewVector(352, 628), 32),
+		sprite:    beholderLeftSprite,
+		transform: pixel.IM.Moved(pixel.V(320, 534)),
+		batch:     beholderBatch,
+		dead:      false,
+	}
+	higherBeholder := beholder{
+		rect:      cirno.NewRectangle(cirno.NewVector(960, 714), 129, 315, 0),
+		hitCircle: cirno.NewCircle(cirno.NewVector(1055, 808), 32),
+		sprite:    beholderRightSprite,
+		transform: pixel.IM.Moved(pixel.V(320, 534)),
+		batch:     beholderBatch,
+		dead:      false,
+	}
+
+	lowerBeholder.rect.SetIdentity(beholderID)
+	lowerBeholder.hitCircle.SetIdentity(beholderEyeID)
+	higherBeholder.rect.SetIdentity(beholderID)
+	higherBeholder.hitCircle.SetIdentity(beholderEyeID)
 
 	// Setup metrics.
 	last := time.Now()
