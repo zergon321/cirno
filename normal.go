25999a2d6c4394a8ea2fd301d2bad76c9f21cdeb
zergon321 <maximgradan@gmail.com>
2020-05-27 19:49:59 +0300

    Normal between circle and rectangle implemented.

--- normal.go
+++ normal.go
@@ -13,6 +13,9 @@ func (circle *Circle) NormalTo(shape Shape) Vector {
 
 	case *Line:
 		return circle.NormalToLine(other)
+
+	case *Rectangle:
+		return circle.NormalToRectangle(other)
 	}
 
 	return Zero
@@ -21,6 +24,11 @@ func (circle *Circle) NormalTo(shape Shape) Vector {
 // NormalTo returns the normal from the given rectangle
 // to the other shape.
 func (rect *Rectangle) NormalTo(shape Shape) Vector {
+	switch other := shape.(type) {
+	case *Circle:
+		return rect.NormalToCircle(other)
+	}
+
 	return Zero
 }
 
@@ -42,6 +50,39 @@ func (line *Line) NormalTo(shape Shape) Vector {
 // to the other circle.
 func (circle *Circle) NormalToCircle(other *Circle) Vector {
 	return other.center.Subtract(circle.center).Normalize()
+}
+
+// NormalToRectangle returns the normal from the given circle
+// to the rectangle.
+func (circle *Circle) NormalToRectangle(rect *Rectangle) Vector {
+	// Transform the circle center coordinates from the world space
+	// to the rectangle's local space.
+	t := circle.center.Subtract(rect.center)
+	theta := -rect.angle
+	t = t.Rotate(theta)
+	localCircle := NewCircle(t, circle.radius)
+	localRect := NewRectangle(NewVector(0, 0), rect.Width(),
+		rect.Height(), 0.0)
+	closestPoint := localCircle.center
+
+	// Find the point of the rectangle which is closest to
+	// the center of the circle.
+	if closestPoint.X < localRect.Min().X {
+		closestPoint.X = localRect.Min().X
+	} else if closestPoint.X > localRect.Max().X {
+		closestPoint.X = localRect.Max().X
+	}
+
+	if closestPoint.Y < localRect.Min().Y {
+		closestPoint.Y = localRect.Min().Y
+	} else if closestPoint.Y > localRect.Max().Y {
+		closestPoint.Y = localRect.Max().Y
+	}
+
+	closestPoint = closestPoint.Rotate(-theta).Add(rect.center)
+	normal := closestPoint.Subtract(circle.center).Normalize()
+
+	return normal
 }
 
 // NormalToLine returns the normal from the given circle
@@ -96,4 +137,10 @@ func (line *Line) NormalToLine(other *Line) Vector {
 	}
 
 	return normal
+}
+
+// NormalToCircle returns the normal from the given rectangle
+// to the circle.
+func (rect *Rectangle) NormalToCircle(circle *Circle) Vector {
+	return circle.NormalToRectangle(rect).MultiplyByScalar(-1)
 }
