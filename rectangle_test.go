be13d64b77ee0b693205d4361405fad771b4a599
zergon321 <maximgradan@gmail.com>
2021-02-11 23:11:17 +0000

    Test rewritten.

--- rectangle_test.go
+++ rectangle_test.go
@@ -7,13 +7,9 @@ import (
 	"github.com/zergon321/cirno"
 )
 
-func TestGetRectangleVertices(t *testing.T) {
-	rect, err := cirno.NewRectangle(cirno.NewVector(18, 13), 4, 4, 45)
-	assert.Nil(t, err)
-	vertices := rect.Vertices()
-
-	assert.Equal(t, cirno.NewVector(18, 10), vertices[0])
-	assert.Equal(t, cirno.NewVector(15, 13), vertices[1])
-	assert.Equal(t, cirno.NewVector(18, 16), vertices[2])
-	assert.Equal(t, cirno.NewVector(21, 13), vertices[3])
+func TestCreateRectangle(t *testing.T) {
+	_, err := cirno.NewRectangle(cirno.NewVector(0, 0), 0, 13, 0)
+	assert.NotNil(t, err)
+	_, err = cirno.NewRectangle(cirno.NewVector(0, 0), 13, 0, 0)
+	assert.NotNil(t, err)
 }
